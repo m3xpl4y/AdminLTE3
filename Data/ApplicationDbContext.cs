9f3ea8d9c627872da3a4be1c661351827af6f0cd
m3xpl4y <m3xpl4y@gmail.com>
2021-11-10 13:15:52 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿namespace AdminLTE3.Data;
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
---
+++ GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using AdminLTE3.Data;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore;

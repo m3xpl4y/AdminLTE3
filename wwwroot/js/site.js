2e555e2a7135576d7b35cfff991f9b0631727af3
m3xpl4y <m3xpl4y@gmail.com>
2021-11-11 08:24:55 +0100

    refactored

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -7,9 +7,46 @@ function darkmodeChange() {
     if (document.getElementById("bodyContainer").classList.contains('dark-mode')) {
 
         document.getElementById("bodyContainer").classList.remove('dark-mode');
+        window.localStorage.setItem("visualMode", "");
+        
+
     }
     else {
         document.getElementById("bodyContainer").classList.add("dark-mode");
+        window.localStorage.setItem("visualMode", "dark-mode");
+       
 
     }
+    //document.getElementById("bodyContainer").classList.toggle("dark-mode");
 }
+
+function localstorageSaveData() {
+    window.localStorage.setItem("visualMode", document.getElementById("bodyContainer").value);
+}
+function alstorageLoadData() {
+
+
+    var darkmode = window.localStorage.getItem("visualMode");
+
+
+    if (darkmode === "dark-mode") {
+
+       
+
+        document.getElementById("bodyContainer").classList.add("dark-mode");
+
+    }
+    else {
+        document.getElementById("bodyContainer").classList.remove('dark-mode');
+       
+
+
+    }
+
+  
+
+    return darkmode;
+   
+}
+
+window.onload = alstorageLoadData;
